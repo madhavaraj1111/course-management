23829dc1b9879ba928c6cee9654e213a789f8b61
madhavaraj1111 <madhavaraj1111@gmail.com>
2025-09-09 19:19:55 +0530

    Initial commit

---
+++ courseClient/README.md
@@ -0,0 +1,7 @@
+1.Project Setup
+
+creation of CourseManagement folder.
+
+inside that courseClient folder then in this npm install , npm install tailwindcss @tailwindcss/vite and in vite.config.js need to import the tailwindcss and add tailwindcss() plugin.
+
+and remove unwanted boiler plate codes and boiler folder and files.Keeping the needed.
---
+++ courseClient/eslint.config.js
@@ -0,0 +1,35 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import prettier from "eslint-config-prettier";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: globals.browser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    plugins: {
+      react: pluginReact,
+      "react-hooks": reactHooks,
+    },
+    extends: [
+      js.configs.recommended,
+      pluginReact.configs.flat.recommended,
+      prettier,
+    ],
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+]);
