6b936a0e538f6b23552dd792a95d1e7e7732847f
madhavaraj1111 <madhavaraj1111@gmail.com>
2025-09-27 02:24:56 +0530

    Free from Auths

--- courseClient/src/routes/AppRoutes.jsx
+++ courseClient/src/routes/AppRoutes.jsx
@@ -6,15 +6,11 @@ import CourseCreate from "../pages/CourseCreate.jsx";
 import NotFound from "../pages/NotFound";
 import MainLayout from "../layouts/MainLayout.jsx";
 import CourseUpdate from "../pages/CourseUpdate.jsx";
-import Login from "../pages/Login.jsx";
 
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
-        {/* Public routes (no MainLayout) */}
-
-        <Route path="/login" element={<Login />} />
         <Route element={<MainLayout />}>
           <Route path="/" element={<Dashboard />} />
           <Route path="courses" element={<CourseList />} />
